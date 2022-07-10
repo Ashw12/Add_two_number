5fef76e45521ba0f1c4b02ca4c141abdfecd09f2
Ashw12 <86472085+Ashw12@users.noreply.github.com>
2022-07-10 15:55:17 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        int number= sc.nextInt();
+        int Number=sc.nextInt();
+        int NUMBER=sc.nextInt();
+        int sum=number+Number+NUMBER;
+        System.out.println(sum);
+
+    }
+}
